229ed0b18ca180f241d8735015ae9ed1bb11475b
fedorks <fksgrupp@ya.ru>
2022-12-08 01:16:49 +0300

    home work 4 done

--- home_work_281122.py
+++ home_work_281122.py
@@ -90,16 +90,16 @@
 # - 3 -> 11
 # - 2 -> 10
 
-# n = int(input('Введите деятичное число: '))
-# n_out = n # используем для красивой печати
-# binary = []
-# x = n
-# while x >= 1:
-#     x = n // 2
-#     binary.append(n - x * 2)
-#     n = x
-# else:
-#     print(f'{n_out} -> ', *binary, sep='')
+""" n = int(input('Введите деятичное число: '))
+n_out = n # используем для красивой печати
+binary = []
+x = n
+while x >= 1:
+    x = n // 2
+    binary.append(n - x * 2)
+    n = x
+else:
+    print(f'{n_out} -> ', *binary, sep='') """
 
 #=========================================================================================
 
@@ -110,14 +110,14 @@
 
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
 
-k = int(input('Введите целое число: '))
-list_fibonachchi = [0] * (2 * k +1)
-list_fibonachchi[k+1] = 1
-list_fibonachchi[k+2] = 1
+# k = int(input('Введите целое число: '))
+# list_fibonachchi = [0] * (2 * k +1)
+# list_fibonachchi[k+1] = 1
+# list_fibonachchi[k+2] = 1
 
-for i in range((k + 3), (k*2+1)):
-    list_fibonachchi[i] = list_fibonachchi[i-1] + list_fibonachchi[i-2]
+# for i in range((k + 3), (k*2+1)):
+#     list_fibonachchi[i] = list_fibonachchi[i-1] + list_fibonachchi[i-2]
     
-for i in range((k - 1), -1, -1):
-    list_fibonachchi[i] = list_fibonachchi[i+2] - list_fibonachchi[i+1]
-print(list_fibonachchi)
+# for i in range((k - 1), -1, -1):
+#     list_fibonachchi[i] = list_fibonachchi[i+2] - list_fibonachchi[i+1]
+# print(list_fibonachchi)
---
+++ homework4_011222.py
@@ -0,0 +1,68 @@
+# Задача 30.
+# Вычислить число c заданной точностью d
+
+# Пример:
+
+# - при d = 0.001, π = 3.141    10^{-1} ≤ d ≤10^{-10}
+
+# import math
+# d = (input('Введите вещественное число 10**(-1) ≤ d ≤ 10**(-10): '))
+# if ',' in d:
+#     d = d.replace(',','.')
+# if '.' in d:
+#     ind = d.index('.')
+# new_pi = round(math.pi, (len(d) - 1 - ind))
+# print(new_pi)
+
+#===============================================================
+
+# Задача 31.
+# Задайте натуральное число N. Напишите программу, 
+# которая составит список простых множителей числа N.
+
+# n = int(input('Введите натуральное число: '))
+
+# out_list = []
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         for j in range(2, i // 2 + 1):
+#             if i % j == 0:
+#                 break
+#         else:
+#             out_list.append(i)
+# print(out_list)
+
+#==================================================
+# # Вариант 2
+# n = int(input('Введите натуральное число: '))
+# out_list = []
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         index = 1
+#         for j in range(2, ( i // 2 + 1)):
+#             if(i % j == 0):
+#                 index = 0
+#                 break
+#         if(index == 1):
+#             out_list.append(i)
+# print(out_list)
+
+#================================================
+
+# Задача 32
+# Задайте последовательность чисел. Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
+
+n = int(input('Введите количество элементов последовательности: '))
+from random import randint
+numb = []
+for i in range(n):
+    numb.append(randint(0, 20))
+
+out_numb = []
+for i in range(len(numb)):
+    if numb[i] not in out_numb:
+        out_numb.append(numb[i])
+print(numb)
+print(out_numb)
+
